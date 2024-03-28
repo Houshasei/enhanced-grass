9da419910de186767b0f5f3abcf291c1ffe5fedf
houshasei <Houshasei@yandex.com>
2024-03-29 05:14:01 +0800

    Update import.sh

--- import.sh
+++ import.sh
@@ -13,8 +13,8 @@ sudo pm2 kill
 sudo pm2 unstartup
 
 # Define the old and new text values with quotes
-old_text='"d0b011d3-51a7-4106-8352-94ec9b341c57"'
-new_text='"35ee8424-06a6-4aac-a211-13303d73c63d"'
+old_text=''
+new_text=''
 
 # Specify the path to the .env file
 env_file="/usr/bin/enhanced-grass/.env"
