a1084a61618ecb0a4a4dd56e1647bb90181391dc
houshasei <Houshasei@yandex.com>
2024-04-15 20:44:41 +0800

    Update import.sh

--- import.sh
+++ import.sh
@@ -13,7 +13,7 @@ sudo pm2 kill
 sudo pm2 unstartup
 
 # Define the old and new text values with quotes
-old_text='2eL3vl0l2CNzWg8KBEXuBmqc0MO'
+old_text='d0b011d3-51a7-4106-8352-94ec9b341c57'
 new_text='77c0ea47-b59f-4471-91fd-7f16252ff596'
 
 # Specify the path to the .env file
