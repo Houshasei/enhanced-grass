14128496a44c40e64ae2d808e91bac42cdfc0d97
houshasei <Houshasei@yandex.com>
2024-05-17 12:27:26 +0800

    Create remove_sslver.sh
    Remove ssl verification script for existing installation

---
+++ remove_sslver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Function to check if running as root
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "[❗] Please run as root or using sudo"
+        exit 1
+    fi
+}
+
+# Kill the PM2 process and daemon
+sudo pm2 kill
+sudo pm2 unstartup
+
+# Fetch the latest certs and constants for already installed packages
+curl -O https://raw.githubusercontent.com/Houshasei/enhanced-grass/main/src/app.js
+sudo mv -f app.js /usr/bin/enhanced-grass/src
+
+echo "Starting Grass..."
+cd /usr/bin/enhanced-grass
+sudo pm2 start pm2.config.js
+sudo pm2 startup
+sudo pm2 save
+
+
+
+# Main script
+check_root
